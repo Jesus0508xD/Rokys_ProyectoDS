72e68ffcccf4b3217e79e2ef70f3225455fdf212
Jesus Alarcon <jesus.alarcon3@unmsm.edu.pe>
2018-11-12 10:52:07 -0500

    Aiudaaaaaa

--- app/controller/LoginController.php
+++ app/controller/LoginController.php
@@ -1,6 +1,6 @@
 <?php
-/*include_once("../app/model/Trabajador.php");
-include_once("../app/service/TrabajadorService.php");*/
+include_once("../app/model/Trabajador.php");
+include_once("../app/service/TrabajadorService.php");
 
 class LoginController extends Controller{
         public function __construct(){}
@@ -11,15 +11,15 @@ class LoginController extends Controller{
 
         public function login(){
             $codigo=$_POST['password'];
-            /*$trabajador_service=new TrabajadorService;
-            $trabajador=$trabajador_service->read($codigo);*/
-            /*if(isset($trabajador)){
+            $trabajador_service=new TrabajadorService;
+            $trabajador=$trabajador_service->read($codigo);
+            if(isset($trabajador)){
                 Session::init();
                 Session::add('Trabajador',$trabajador);
                 header('location: ../HomeController');
             }else{
                 die('Usuario no registrado');
-            }*/
-            echo $codigo;
+            }
+            
         }
 }
